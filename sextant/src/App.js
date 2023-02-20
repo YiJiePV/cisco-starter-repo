0c9dd61e802644cd8107d833373e38317bf43c06
Karena Qian <qiank@spu.edu>
2023-02-19 17:48:51 -0800

    Deleted files and created banner and exhibit comps

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+function Banner(props){
+  return <h1>{props.title}</h1>;
+}
+function Exhibit(props){
+  return(
+    <div className={'Exhibit Exhibit-' + props.color} height={props.height} width={props.width}>
+      <h2>{props.heading}</h2>
+      {props.children}
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Banner title="Sextant Dashboard"/>
+      <Exhibit color="blue"
+                height="500px" 
+                width="500px" 
+                heading="Heading Title Goes Here">
+        <p>Put Children Here</p>
+      </Exhibit>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +28,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
